ed88e25a686bf4dd3fb79a9a08381e2d2aaab5b3
Anthony Giacalone <agiacalone@gmail.com>
2013-02-14 18:03:56 -0800

    some minor changes and comment updates

--- qroot.f
+++ qroot.f
@@ -22,13 +22,24 @@ C         For backwards compatability (and to keep my project as
 C         historically accurate as possible) I've chosen to keep
 C         with tradition and use the WRITE (*,*) whenever
 C         possible.
+C
+C      3. There are (at least) two different ways to do power
+C         arithmetic: 2**2 and SQRT(2). Both are built-in
+C         functions, unlike with most modern languages. Also,
+C         there are functions to perform arithmetic on imaginary
+C         numbers (also built-in), some of which were put into use
+C         in this program like CONJ (CONJ calculates the complex
+C         conjugate of a complex number). Given the vast amount of
+C         built-in mathematical operations in Fortran, it appears
+C         to be written and intended for heavy math and 
+C         engineering use.
 
 
 C 3456789112345678921234567893123456789412345678951234567896123456
       PROGRAM QROOT
-      REAL a,b,c,e,rroot_a,rroot_b,discr
       CHARACTER restart
       COMPLEX croot_a,croot_b
+      REAL a,b,c,e,rroot_a,rroot_b,discr
 
       WRITE (*,*) 'This program calculates the real or complex'
       WRITE (*,*) '(imaginary) roots of a quadratic equation'
@@ -37,7 +48,7 @@ C 3456789112345678921234567893123456789412345678951234567896123456
       READ (*,*) a,b,c
 
       e = 1.0e-9
-      discr = b*b - 4.0*a*c
+      discr = b * b - 4.0 * a * c
 
       IF (ABS(discr) < e) THEN
           rroot_a = -b / (2.0 * a)
@@ -52,7 +63,7 @@ C 3456789112345678921234567893123456789412345678951234567896123456
           WRITE (*,*) "Root B = ", rroot_b
 
       ELSE
-          croot_a = (-b + SQRT(CMPLX(discr))) / (2.0*a) 
+          croot_a = (-b + SQRT(CMPLX(discr))) / (2.0 * a) 
           croot_b = CONJG(croot_a)
           WRITE (*,*) "Equation has two imaginary roots:" 
           WRITE (*,*) "Root A = ", croot_a
@@ -67,3 +78,4 @@ C         GOTOs are bad. Don't ever use them.
           GOTO 100
       END IF
       END
+
